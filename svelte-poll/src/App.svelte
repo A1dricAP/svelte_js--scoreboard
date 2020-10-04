08af5eba5ed7f94c4ea8e55550d13f779baac110
Aldric Pereira <52449719+A1dricAP@users.noreply.github.com>
2020-10-04 23:09:37 +0530

    #4 - User Input & Data Binding

--- svelte-poll/src/App.svelte
+++ svelte-poll/src/App.svelte
@@ -1,9 +1,16 @@
 <script>
+  import { text } from "svelte/internal";
+
   let name = "aldric_ap";
   let beltColour = "black";
 
   const handleClick = () => {
     beltColour = "orange";
+  };
+
+  //* taking e as the event parameter.
+  const handleInput = (e) => {
+    beltColour = e.target.value; // target being the field, where an action has taken place and value being the value of the target.
   };
 </script>
 
@@ -31,6 +38,12 @@
 
 <main>
   <h1>Hello {name}!</h1>
-  <p>{beltColour} belt!</p>
+  <p style="color:{beltColour}">{beltColour} belt!</p>
   <button on:click={handleClick}>Update colour!</button>
+
+  <!-- for the input field, "value" is to set the default value of the input box, in this case, the beltColour value. -->
+  <input type="text" on:input={handleInput}  />
+
+  <!-- this input field demos two-way binding. With the help of the "bind" keyword. -->
+  <input type="text" bind:value={beltColour} />
 </main>
