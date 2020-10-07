a880c4816a686ae79998a75d05121d1ca8ac2e45
Aldric Pereira <52449719+A1dricAP@users.noreply.github.com>
2020-10-07 23:13:46 +0530

    #19 - Header & Footer Components

--- pollapp/src/App.svelte
+++ pollapp/src/App.svelte
@@ -1,9 +1,15 @@
 <script>
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/footer.svelte";
 </script>
 
 <style>
+  main {
+    max-width: 960;
+    margin: 40px auto;
+  }
 </style>
 
-<main>
-  <h1>Hello, People!</h1>
-</main>
+<Header />
+<main />
+<Footer />
---
+++ pollapp/src/components/footer.svelte
@@ -0,0 +1,18 @@
+<style>
+  footer {
+    padding: 40px;
+    text-align: center;
+  }
+
+  .copyright {
+    color: rgb(117, 117, 117);
+    font-size: 14px;
+    display: inline-block;
+    padding: 20px;
+    border-top: 1px solid #d61c1c;
+  }
+</style>
+
+<footer>
+  <div class="copyright">Copyright 2020 Polling.</div>
+</footer>
